534dd6718aff5a21b6b35706c41390ab6ee29b79
Timothy <t.m.martel@gmail.com>
2024-02-21 16:38:41 -0500

    type-annotations

--- demo/type-annotations/type-annotations.ts
+++ demo/type-annotations/type-annotations.ts
@@ -9,3 +9,21 @@ import { strict as assert } from "assert";
 //
 // Useful links:
 // https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#type-annotations-on-variables
+
+const myName = "Timothy";
+const amount: number = 2+2;
+const hello: string = `Hello {myName}`;
+const powerLevel: bigint = 9000n + 1n;
+const yes: boolean = true;
+
+//always include type annotations in parameters and return type
+function sum(lhs: number, rhs: number): number {
+    const total = lhs + rhs;
+    return total;
+}
+
+const one = sum(1, 0);
+console.log(one);
+
+const two = sum(1, 1);
+console.log(two);
